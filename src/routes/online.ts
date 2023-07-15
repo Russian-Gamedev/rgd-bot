c53bb217d28e8bcb57518d15a152d04f1b487f11
Damir Lutfrakhmanov <d.amlutstudio@gmail.com>
2023-07-16 02:22:40 +0500

    :sparkles:feat: корректный счетчик онлайн сервера

--- src/routes/online.ts
+++ src/routes/online.ts
@@ -2,25 +2,25 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { ApiRequest, ApiResponse, methods, Route } from '@sapphire/plugin-api';
 import { Time } from '@sapphire/time-utilities';
 
-import { RGD_SERVER_ID } from '@/configs/constants';
-
 @ApplyOptions<Route.Options>({
   route: '/online',
 })
 export class OnlineRoute extends Route {
   private cache = {
     online: 0,
-    total: 0,
+    members: 0,
   };
   private cacheNext = 0;
 
   public async [methods.GET](_request: ApiRequest, response: ApiResponse) {
     if (Date.now() - this.cacheNext > 0) {
-      const server = await this.container.client.guilds.fetch(RGD_SERVER_ID);
-      this.cache.total = server.members.cache.size;
-      this.cache.online = server.members.cache.filter(
-        (member) => (member?.presence?.status ?? 'offline') != 'offline',
-      ).size;
+      const response = await fetch(
+        'https://discord.com/api/v9/invites/5kZhhWD?with_counts=true',
+      ).then((res) => res.json());
+
+      this.cache.online = response.approximate_member_count;
+      this.cache.members = response.approximate_presence_count;
+
       this.cacheNext = Date.now() + Time.Minute * 5;
     }
 
