03273f38bdb1d32be97a8dc942b43d58a4058cee
Damir Lutfrakhmanov <d.amlutstudio@gmail.com>
2023-07-23 17:25:13 +0500

    :sparkles:feat: clyde знает кто пишет

--- src/listeners/clyde/clyde-messages.ts
+++ src/listeners/clyde/clyde-messages.ts
@@ -20,8 +20,41 @@ export class ClydeMessages extends Listener {
         text += '\n\n' + reference.content;
       }
       await message.channel.sendTyping();
+
+      text = await this.mentionToDisplayName(text);
+      text = this.normalizeServerEmoji(text);
+
+      text = `Пользователь ${message.member.user.username} пишет: ` + text;
+
       const response = await this.container.clyde.send(text);
       await message.reply(response);
     }
+  }
+
+  private normalizeServerEmoji(text: string) {
+    const regexEmoji = /:(\w{2,32}):|<a?:\w{2,32}:\d{17,20}>/g;
+
+    for (const [emoji_text, emoji] of text.matchAll(regexEmoji)) {
+      if (emoji_text.startsWith('<')) {
+        const emoji_id = this.container.client.emojis.resolve(emoji);
+        if (emoji_id) {
+          text = text.replaceAll(emoji_text, '\\' + emoji_id.toString());
+        }
+      } else {
+        text = text.replaceAll(emoji_text, '\\' + emoji_text);
+      }
+    }
+    return text;
+  }
+
+  private async mentionToDisplayName(text: string) {
+    for (const mention of text.matchAll(/<@!?(?<id>\d{17,20})>/g)) {
+      const [mention_ping, id] = mention;
+      if (!id) continue;
+      const member = await this.container.rgd.members.fetch(id);
+
+      text = text.replaceAll(mention_ping, member.displayName);
+    }
+    return text;
   }
 }
