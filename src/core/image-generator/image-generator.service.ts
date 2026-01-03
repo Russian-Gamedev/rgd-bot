f7177284c88947f55168b85870d4755aff5f08e5
DamirLut <lutfrahmanovdamir3@gmail.com>
2026-01-03 10:34:06 +0300

    chore: return banner generation

--- src/core/image-generator/image-generator.service.ts
+++ src/core/image-generator/image-generator.service.ts
@@ -1,4 +1,4 @@
-import { ForbiddenException, Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { Renderer } from '@takumi-rs/core';
 import { fromJsx } from '@takumi-rs/helpers/jsx';
 import { Client } from 'discord.js';
@@ -6,6 +6,8 @@ import Redis from 'ioredis';
 import path from 'path';
 
 import { GuildService } from '#core/guilds/guild.service';
+
+import { renderInviteBanner } from './renderers/invite-banner';
 
 export interface ImageRenderer {
   node: Awaited<ReturnType<typeof fromJsx>>;
@@ -46,8 +48,50 @@ export class ImageGeneratorService {
   }
 
   public async renderInviteBanner(inviteCode: string): Promise<Buffer> {
-    console.log(inviteCode);
-    throw new ForbiddenException('Invite banners are disabled');
+    this.logger.debug(`Requested render invite banner for code: ${inviteCode}`);
+    const cache = this.cacheImage(`invite-banner:${inviteCode}`, 3600);
+    const cachedImage = await cache.get();
+    if (cachedImage) {
+      this.logger.debug(`Cache hit for invite banner: ${inviteCode}`);
+      return cachedImage;
+    }
+
+    const time = Date.now();
+    this.logger.debug(`Cache miss for invite banner: ${inviteCode}`);
+
+    const invite = await this.discord.fetchInvite(inviteCode).catch(() => null);
+
+    if (!invite?.guild) {
+      throw new NotFoundException('Invite not found');
+    }
+
+    const guild = await this.guildService.getGuildById(invite.guild.id);
+
+    const title = invite.guild.name;
+    const iconURL = invite.guild.iconURL({ size: 128 }) ?? '';
+    const members = invite.memberCount ?? 0;
+    const online = invite.presenceCount ?? 0;
+    const banner = guild?.custom_banner_url ?? invite.guild.bannerURL();
+
+    const { width, height, node } = await renderInviteBanner({
+      title,
+      iconURL,
+      members,
+      online,
+      banner,
+    });
+    const buffer = await this.renderer.render(node, {
+      width,
+      height,
+      format: 'webp',
+    });
+    await cache.set(buffer);
+    this.logger.debug(
+      `Rendered invite banner for code: ${inviteCode} in ${
+        Date.now() - time
+      }ms`,
+    );
+    return buffer;
   }
 
   private cacheImage(key: string, ttl: number) {
