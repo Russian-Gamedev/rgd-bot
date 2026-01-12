9dcf0d64e3cdbc5ab578c427d9f77e54e2a82fd1
Damir Lutfrakhmanov <lutfrahmanovdamir3@gmail.com>
2026-01-12 08:23:51 +0300

    fix: Add polyfill for writable Error.message property

--- src/lib/polyfill.ts
+++ src/lib/polyfill.ts
@@ -2,3 +2,6 @@
 BigInt.prototype.toJSON = function () {
   return this.toString();
 };
+
+// fixes Bun standalone executable Error#message being non-writable
+Object.defineProperty(Error.prototype, "message", { writable: true, configurable: true });
